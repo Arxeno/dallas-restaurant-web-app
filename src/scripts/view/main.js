c78c701b24a892a3024f63cf6a82a57725df0ae2
raisyam <rmfbkw04@gmail.com>
2022-11-21 10:13:13 +0700

    import file json dan coba console.log-kan

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,5 +1,8 @@
+import dataJson from "../../DATA.json" assert { type: "json" };
+
 const main = () => {
   console.log("Hello Coders! :)");
+  console.log(dataJson.restaurants[0]);
 };
 
 export default main;
