8a2cfeeddf03e249a7d1ffbc66877a20d06538d6
raisyam <rmfbkw04@gmail.com>
2022-11-21 10:15:37 +0700

    masih coba tes console log dataJson nya

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -3,6 +3,10 @@ import dataJson from "../../DATA.json" assert { type: "json" };
 const main = () => {
   console.log("Hello Coders! :)");
   console.log(dataJson.restaurants[0]);
+
+  for (let i = 0; i < dataJson.restaurants.length; i++) {
+    console.log(dataJson.restaurants[i]);
+  }
 };
 
 export default main;
