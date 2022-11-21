a43ad79c5f8ad5c223053ef5c8a8683d3ed6e3a1
raisyam <rmfbkw04@gmail.com>
2022-11-21 10:32:17 +0700

    nampilin dataJson menjadi card-card di HTML (pake for loop)

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,12 +1,42 @@
 import dataJson from "../../DATA.json" assert { type: "json" };
 
 const main = () => {
-  console.log("Hello Coders! :)");
-  console.log(dataJson.restaurants[0]);
+  // console.log("Hello Coders! :)");
+  // console.log(dataJson.restaurants[0]);
+
+  const footer = document.querySelector("footer");
+  document.body.removeChild(footer);
 
   for (let i = 0; i < dataJson.restaurants.length; i++) {
-    console.log(dataJson.restaurants[i]);
+    const id = dataJson.restaurants[i].id;
+    const name = dataJson.restaurants[i].name;
+    const description = dataJson.restaurants[i].description;
+    const pictureUrl = dataJson.restaurants[i].pictureId;
+    const city = dataJson.restaurants[i].city;
+    const rating = dataJson.restaurants[i].rating;
+
+    const card = `
+    <div id="${id}" class="card">
+      <img
+        src="${pictureUrl}"
+        alt="restaurant photo"
+      />
+      <div class="card-title"><p>${name}, ${city}</p></div>
+      <div class="card-content-container">
+        <div class="rating">
+          <h1>${rating}</h1>
+          <div><p>Rating</p></div>
+        </div>
+        <p class="description">
+          ${description.slice(0, 72)}...
+        </p>
+      </div>
+    </div>`;
+
+    document.body.innerHTML += card;
   }
+
+  document.body.appendChild(footer);
 };
 
 export default main;
