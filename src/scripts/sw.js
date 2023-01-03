35ac8b57321d840c90038061d50b77af7d6c9949
raisyam <rmfbkw04@gmail.com>
2023-01-03 22:06:26 +0700

    menerapkan fungsi-fungsi cache-helper.js pada sw.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,39 @@
+import CacheHelper from './utils/cache-helper';
+import 'regenerator-runtime';
+
+const assetsToCache = [
+  './',
+  './images/heros/hero-image_1.jpg',
+  './images/heros/hero-image_2.jpg',
+  './images/heros/hero-image_3.jpg',
+  './images/heros/hero-image_4.jpg',
+  './images/icons/icon-72x72.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-512x512.png',
+  './images/favicon.png',
+  './images/hamburger-menu.png',
+  './index.html',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker ...');
-
-  // TODO: Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
   console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
