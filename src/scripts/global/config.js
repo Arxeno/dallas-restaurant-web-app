8be9a5b9a6eb49362ca178225443d49ca741fac1
raisyam <rmfbkw04@gmail.com>
2023-01-03 22:27:02 +0700

    Menyiapkan Fungsi Operations DB

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,6 +2,9 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   CACHE_NAME: 'DallasRestaurantApp-V1',
+  DATABASE_NAME: 'dallas-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
