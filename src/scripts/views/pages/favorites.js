4ffba15e339d8ef6839860c0ef0eaad4357547ae
raisyam <rmfbkw04@gmail.com>
2023-01-04 12:32:16 +0700

    menampilkan restaurant yg di favoritkan di laman Favorites (dan membuat fungsi callback nya juga)

--- src/scripts/views/pages/favorites.js
+++ src/scripts/views/pages/favorites.js
@@ -1,14 +1,28 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import TemplateCreator from '../templates/template-creator';
+
 const Favorites = {
   async render() {
     return `
-    <div class="content">
-      <h1 id="head-title-restaurant">Favorites</h1>
-      <section id="card-container"></section>
+    <div class='content'>
+      <h1 id='head-title-restaurant'>Favorites</h1>
+      <section id='card-container'></section>
     </div>  
     `;
   },
 
-  async afterRender() {},
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const cardContainer = document.querySelector('#card-container');
+
+    if (restaurants.length > 0) {
+      restaurants.forEach((restaurant) => {
+        cardContainer.innerHTML += TemplateCreator.createRestaurantCardTemplate(restaurant);
+      });
+    } else {
+      cardContainer.innerHTML = '<p class="center-text">You don\'t have any favorited restaurant<p>';
+    }
+  },
 };
 
 export default Favorites;
