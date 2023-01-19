efa3f9da26983cd22c87754a835ea34be9cc5a88
raisyam <rmfbkw04@gmail.com>
2023-01-19 10:12:04 +0700

    menambahkan tag <picture> dan tag <source>. Menerapkan image responsive

--- src/scripts/views/pages/now-available.js
+++ src/scripts/views/pages/now-available.js
@@ -4,11 +4,15 @@ import TemplateCreator from '../templates/template-creator';
 const NowAvailable = {
   async render() {
     return `
-    <img
-        id="hero-image"
-        src="./images/heros/hero-image_1.jpg"
-        alt="people cooking"
-    />
+    <picture>
+      <source media="(max-width: 425px)" srcset="./images/heros/hero-image_1-small.jpg">
+      <source media="(max-width: 768px)" srcset="./images/heros/hero-image_1-medium.jpg">
+      <img
+            id="hero-image"
+            src="./images/heros/hero-image_1-large.jpg"
+            alt="people cooking"
+        />
+    </picture>
     <div class="content">
       <h1 id="head-title-restaurant">Explore The World of Food</h1>
       <section id="card-container"></section>
