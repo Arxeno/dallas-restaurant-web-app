24545dce640ce4c9fd6c8c38a29b62c729b1b1d8
raisyam <rmfbkw04@gmail.com>
2023-01-21 03:22:11 +0700

    rename likeRestaurantSpec.js

---
+++ specs/favoriteRestaurantSpec.js
@@ -0,0 +1,13 @@
+import FavoriteButtonInitiator from '../src/scripts/utils/favorite-button-initiator';
+
+describe('Favoriting A Restaurant', () => {
+  it('should show the favorite button when the restaurant has not been favorited before', async () => {});
+
+  it('should not show the unfavorite button when the restaurant has not been favorited before', async () => {});
+
+  it('should be able to favorite the restaurant', async () => {});
+
+  it('should not add a restaurant again when its already favorited', async () => {});
+
+  it('should not add a restaurant when it has no id', async () => {});
+});
