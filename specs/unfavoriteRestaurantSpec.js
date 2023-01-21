6d20c26d1f9cf8dbaf68ca5502b035a729d26a7f
raisyam <rmfbkw04@gmail.com>
2023-01-21 20:05:47 +0700

    yuu bisa yuu selesai

--- specs/unfavoriteRestaurantSpec.js
+++ specs/unfavoriteRestaurantSpec.js
@@ -1,11 +1,17 @@
 import FavoriteButtonInitiator from '../src/scripts/utils/favorite-button-initiator';
 import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
 
-describe('Unfavoriting A Restaurant', () => {
-  it('should display unfavorite widget when the restaurant has been favorited', async () => {
-    document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
+const addFavoriteButtonContainer = () => {
+  document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
+};
 
-    FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+describe('Unfavoriting A Restaurant', () => {
+  beforeEach(async () => {
+    addFavoriteButtonContainer();
+  });
+
+  it('should display unfavorite widget when the restaurant has been favorited', async () => {
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
 
     await FavoriteButtonInitiator.init({
       buttonContainer: document.querySelector('#favoriteButtonContainer'),
@@ -16,13 +22,11 @@ describe('Unfavoriting A Restaurant', () => {
 
     expect(document.querySelector('[aria-label="delete restaurant from favorite"]')).toBeTruthy();
 
-    FavoriteRestaurantIdb.deleteRestaurant(1);
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
   it('should not display favorite widget when the restaurant has been favorited', async () => {
-    document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
-
-    FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
 
     await FavoriteButtonInitiator.init({
       buttonContainer: document.querySelector('#favoriteButtonContainer'),
@@ -33,13 +37,11 @@ describe('Unfavoriting A Restaurant', () => {
 
     expect(document.querySelector('[aria-label="put restaurant to favorite"]')).toBeFalsy();
 
-    FavoriteRestaurantIdb.deleteRestaurant(1);
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
   it('should be able to remove favorited restaurant from the list', async () => {
-    document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
-
-    FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
 
     await FavoriteButtonInitiator.init({
       buttonContainer: document.querySelector('#favoriteButtonContainer'),
@@ -52,8 +54,23 @@ describe('Unfavoriting A Restaurant', () => {
 
     expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
 
-    FavoriteRestaurantIdb.deleteRestaurant(1);
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
-  it('should not throw error if the unfavorited restaurant is not in the list', async () => {});
+  it('should not throw error if the unfavorited restaurant is not in the list', async () => {
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+
+    await FavoriteButtonInitiator.init({
+      buttonContainer: document.querySelector('#favoriteButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    document.querySelector('[aria-label="delete restaurant from favorite"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
 });
